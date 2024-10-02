fabc32c2977bbe300484de739f498772bf13d1fd
Giang Hoang <giang.hoang@electrolux.com>
2024-10-02 16:27:00 +0700

    release

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,9 @@
 import "./input.scss";
 import { fetchData } from "../../utils/fetch-data";
-import { debounce } from "../../utils/deboucne";
+import { debounce } from "../../utils/debounce";
 import Loader from "../Loader";
+import { ChangeEvent, useState } from "react";
+import List from "../List";
 
 export interface InputProps {
   /** Placeholder of the input */
@@ -12,12 +14,60 @@ export interface InputProps {
 
 const Input = ({ placeholder, onSelectItem }: InputProps) => {
   // DO NOT remove this log
-  console.log('input re-render')
+  console.log("input re-render");
 
   // Your code start here
-  return <input></input>
+  const [isLoading, setIsLoading] = useState(false);
+  const [results, setResults] = useState<string[]>();
+  const [message, setMessage] = useState("");
+  const [isInit, setIsInit] = useState(true);
+
+  const handleOnChange = debounce(
+    async (event: ChangeEvent<HTMLInputElement>) => {
+      const searchText = event.target.value;
+
+      if (searchText) {
+        setIsInit(false);
+        setIsLoading(true);
+
+        try {
+          const resultList = await fetchData(searchText);
+          setResults(resultList);
+
+          if (resultList.length === 0) {
+            setMessage("No results");
+          }
+        } catch (e: any) {
+          setMessage(e);
+        }
+      } else {
+        setResults([]);
+        setIsInit(true);
+      }
+
+      setIsLoading(false);
+    },
+    300
+  );
+
+  return (
+    <div className="input-search-container">
+      <input placeholder={placeholder} onChange={handleOnChange}></input>
+
+      {!isInit && (isLoading || results) ? (
+        <div className="search-result">
+          {isLoading ? <Loader></Loader> : null}
+          <List
+            handleSelectItem={onSelectItem}
+            listItem={results}
+            isLoading={isLoading}
+            message={message}
+          ></List>
+        </div>
+      ) : null}
+    </div>
+  );
   // Your code end here
 };
 
 export default Input;
-
